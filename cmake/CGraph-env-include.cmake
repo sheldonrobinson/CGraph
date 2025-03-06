2206d07e86ed22730811fe84c259696712cab523
Sheldon Robinson <sheldon.robinson@gmail.com>
2025-03-06 13:44:14 -0500

    Update cmake to build static and shared libs

--- cmake/CGraph-env-include.cmake
+++ cmake/CGraph-env-include.cmake
@@ -4,8 +4,10 @@
 # 根据当前 CGraph-env-include.cmake 文件的位置，定位CGraph整体工程的路径
 # 从而解决并兼容了直接引入和三方库引入的路径不匹配问题
 get_filename_component(CGRAPH_PROJECT_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" DIRECTORY)
-set(CGRAPH_PROJECT_ROOT_DIR "${CGRAPH_PROJECT_CMAKE_DIR}/../")
+set(CGRAPH_PROJECT_ROOT_DIR "${CGRAPH_PROJECT_CMAKE_DIR}/..")
+
 file(GLOB_RECURSE CGRAPH_PROJECT_SRC_LIST "${CGRAPH_PROJECT_ROOT_DIR}/src/*.cpp")
+file(GLOB_RECURSE CGRAPH_PROJECT_HDR_LIST "${CGRAPH_PROJECT_ROOT_DIR}/src/*.h" "${CGRAPH_PROJECT_ROOT_DIR}/src/*.inl")
 
 IF(APPLE)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m64 -O2 \
@@ -33,6 +35,38 @@ ENDIF()
 include_directories(${CGRAPH_PROJECT_ROOT_DIR}/src/)    # 直接加入"CGraph.h"文件对应的位置
 
 # 以下三选一，本地编译执行，推荐OBJECT方式
-add_library(CGraph OBJECT ${CGRAPH_PROJECT_SRC_LIST})      # 通过代码编译
+add_library(CGraphObjs OBJECT ${CGRAPH_PROJECT_SRC_LIST})      # 通过代码编译
 # add_library(CGraph SHARED ${CGRAPH_PROJECT_SRC_LIST})    # 编译libCGraph动态库
 # add_library(CGraph STATIC ${CGRAPH_PROJECT_SRC_LIST})    # 编译libCGraph静态库
+
+if(BUILD_SHARED_LIBS)
+  add_library(CGraph SHARED $<TARGET_OBJECTS:CGraphObjs>) 
+else()
+  add_library(CGraph STATIC $<TARGET_OBJECTS:CGraphObjs>)
+endif()
+
+if(NOT SKIP_INSTALL_ALL )
+set(INSTALL_INC_DIR "${CMAKE_INSTALL_PREFIX}/include" CACHE PATH "Installation directory for headers")
+foreach ( file ${CGRAPH_PROJECT_HDR_LIST} )
+    get_filename_component( dir ${file} DIRECTORY )
+	string(REGEX REPLACE "^${CGRAPH_PROJECT_ROOT_DIR}/src[/]?" "" output_path "${dir}")
+    install(FILES ${file} DESTINATION "${INSTALL_INC_DIR}/CGraph/${output_path}" )
+endforeach()
+
+install(TARGETS CGraph
+	EXPORT CGraph-targets
+	RUNTIME DESTINATION bin
+	ARCHIVE DESTINATION lib
+	LIBRARY DESTINATION lib
+	INCLUDES DESTINATION include/CGraph
+	PUBLIC_HEADER DESTINATION include/CGraph
+)
+
+
+
+install(EXPORT CGraph-targets
+    FILE CGraphConfig.cmake
+    DESTINATION lib/cmake/CGraph
+)
+endif()
+
